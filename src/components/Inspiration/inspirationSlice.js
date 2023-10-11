c3268ca6c37d11f7c6359fc2014d5ada9c48ad88
Tim DeCillis <timdecillis@gmail.com>
2023-10-10 17:57:01 -0700

    Created inspirationSlice and all accompanying logic

---
+++ src/components/Inspiration/inspirationSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  inspirations: ['Lady Gaga', 'Dolly Parton', 'Madonna'],
+  userType: 'undecided'
+}
+
+const inspirationSlice = createSlice({
+  name: 'inspiration',
+  initialState,
+  reducers: {
+    inspirationAdded(state, action) {
+      const { inspiration } = action.payload
+        state.types.push(inspiration)
+        return
+      },
+    inspirationChosen(state, action) {
+      const { inspiration } = action.payload
+      state.userType = inspiration
+    }
+  }
+})
+
+export default inspirationSlice.reducer
+
+export const { inspirationAdded, inspirationChosen } = inspirationSlice.actions
+
+export const getInspirations = state => state.inspirations
