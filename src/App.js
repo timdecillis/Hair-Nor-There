f31e55685c533cae58153351b217f1e29fdc4f2c
Tim DeCillis <timdecillis@gmail.com>
2023-10-11 13:51:49 -0700

    Added Suggestion to App, Generate Button also

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 import HairType from './components/HairType/HairType'
 import Inspiration from './components/Inspiration/Inspiration'
+import Suggestion from './components/Suggestion/Suggestion'
 
 const App = () => {
   return (
     <div>
       <HairType />
       <Inspiration />
+      <button>Generat Suggestion</button>
+      <Suggestion/>
     </div>
   )
 }
