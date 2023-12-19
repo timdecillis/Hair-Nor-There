8f00d9308e1a5b8457d0f0fed6316a88071461c5
Tim DeCillis <timdecillis@gmail.com>
2023-12-18 18:25:55 -0800

    Updated reducer

--- src/reducer.ts
+++ src/reducer.ts
@@ -4,6 +4,12 @@ export const reducer = (state = initialState, action: any) => {
   if (action.type === 'INCREMENT') {
     return {value: state.value + 1}
   }
+  if (action.type === 'DECREMENT') {
+    return {value: state.value - 1}
+  }
+  if (action.type === 'SET') {
+    return {value: action.payload}
+  }
   return state;
 }
 
